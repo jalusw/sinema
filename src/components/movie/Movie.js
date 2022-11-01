ba800f780a2b6b54744a4dc8a76f153a94ddecfb
jalusatriani981@gmail.com <jalusw>
2022-11-01 21:52:37 +0800

    fix

--- src/components/movie/Movie.js
+++ src/components/movie/Movie.js
@@ -15,8 +15,8 @@ export default class Movie extends HTMLElement {
 
   connectedCallback() {
     this.innerHTML = `
-    <a href="#" class="group block relative min-w-[150px] max-w-[200px] overflow-hidden" aria-label="${this._movie.original_title}">
-      <img src="https://image.tmdb.org/t/p/w220_and_h330_face/${this._movie.poster_path}" class="rounded" loading="lazy" alt="" />
+    <a href="#" class="group block relative min-w-[150px]   overflow-hidden" aria-label="${this._movie.original_title}">
+      <img src="https://image.tmdb.org/t/p/w220_and_h330_face/${this._movie.poster_path}" class="rounded" loading="lazy" alt="" width="100%"/>
       <div class="absolute top-0 bottom-0 left-0 right-0 w-full h-full bg-[rgba(0,0,0,0.50)] z-[2] hidden group-hover:block"></div>
       <div class="absolute top-0  left-0  w-full h-full  py-1 px-2 flex flex-col justify-end scale-100 z-[3] translate-y-full group-hover:translate-y-0 transition duration-300">
         <b class="line-clamp-1 text-slate-100">${this._movie.original_title}</b>
